57949d49c781443da0cb8dd5f6e604d4c7d7639e
Geo <tp53.geo@gmail.com>
2025-02-06 04:46:03 +0100

    refactor to use normal func instead of lambdas

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ This project transforms an SD card reader into a functional USB stick, the progr
 - **USB Mass Storage Device**: Enables the SD card to function as a standard USB stick.
 - **Plug and Play**: Automatically mounts the SD card when connected to a USB host.
 
+**Note:** Transfer speeds are quite low due to the limitation of the SD card reader
+
 ## Requirements
 
 - **Hardware**:
--- src/main.cpp
+++ src/main.cpp
@@ -66,64 +66,61 @@ void displayMessage(std::string message) {
     M5.Lcd.print(message.c_str());
 }
 
-void setup_usb_callback() {
-  // Write Callback
-  auto onWrite = [](uint32_t lba, uint32_t offset, uint8_t* buffer, uint32_t bufsize) -> int32_t {
-    // Verify free space
+int32_t usbWriteCallback(uint32_t lba, uint32_t offset, uint8_t* buffer, uint32_t bufsize) {
+    // Verify freespace
     uint64_t freeSpace = SD.totalBytes() - SD.usedBytes();
     if (bufsize > freeSpace) {
-      return -1; // no space available
+        return -1; // no space available
     }
 
     // Verify sector size
     const uint32_t secSize = SD.sectorSize();
     if (secSize == 0) return -1;  // disk error
 
-    // For each sector in the buffer, write
+    // Write blocs
     for (uint32_t x = 0; x < bufsize / secSize; ++x) {
-      uint8_t blkBuffer[secSize];
-      memcpy(blkBuffer, buffer + secSize * x, secSize);
-      if (!SD.writeRAW(blkBuffer, lba + x)) {
-        return -1;
-      }
+        uint8_t blkBuffer[secSize];
+        memcpy(blkBuffer, buffer + secSize * x, secSize);
+        if (!SD.writeRAW(blkBuffer, lba + x)) {
+            return -1;  // write error
+        }
     }
     return bufsize;
-  };
+}
 
-  // Read Callback
-  auto onRead = [](uint32_t lba, uint32_t offset, void* buffer, uint32_t bufsize) -> int32_t {
+int32_t usbReadCallback(uint32_t lba, uint32_t offset, void* buffer, uint32_t bufsize) {
     // Verify sector size
     const uint32_t secSize = SD.sectorSize();
     if (secSize == 0) return -1;  // disk error
 
-    // For each sector in the buffer, read
+    // Read blocs
     for (uint32_t x = 0; x < bufsize / secSize; ++x) {
-      if (!SD.readRAW(reinterpret_cast<uint8_t*>(buffer) + (x * secSize), lba + x)) {
-        return -1; // disk errror
-      }
+        if (!SD.readRAW(reinterpret_cast<uint8_t*>(buffer) + (x * secSize), lba + x)) {
+            return -1;  // read error
+        }
     }
     return bufsize;
-  };
+}
 
-  // Start/Stop Callback
-  auto onStartStop = [](uint8_t power_condition, bool start, bool load_eject) -> bool {
+bool usbStartStopCallback(uint8_t power_condition, bool start, bool load_eject) {
     return true;
-  };
+}
 
+void setupUsbCallback() {
   // USB MSC
   uint32_t secSize = SD.sectorSize();
   uint32_t numSectors = SD.numSectors();
   msc.vendorID("ESP32");
   msc.productID("USB_MSC");
   msc.productRevision("1.0");
-  msc.onRead(onRead);
-  msc.onWrite(onWrite);
-  msc.onStartStop(onStartStop);
+  msc.onRead(usbReadCallback);
+  msc.onWrite(usbWriteCallback);
+  msc.onStartStop(usbStartStopCallback);
   msc.mediaPresent(true);
   msc.begin(numSectors, secSize);
 }
 
-void setup_usb_event() {
+void setupUsbEvent() {
   USB.onEvent([](void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
     if (event_base == ARDUINO_USB_EVENTS) {
       auto* data = reinterpret_cast<arduino_usb_event_data_t*>(event_data);
@@ -164,8 +161,8 @@ void setup() {
   }
 
   // USB
-  setup_usb_callback();
-  setup_usb_event();
+  setupUsbCallback();
+  setupUsbEvent();
   displayMessage("USB Stick is ready");
   drawUSBStickIcon();
   drawLedIndicator(false);
